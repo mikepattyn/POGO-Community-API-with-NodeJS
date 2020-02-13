7de602129de2a65ea3428b2f22a478e9cd5a861f
Mike Pattyn <mike@givtapp.net>
2020-02-13 18:32:45 +0100

    try catch verifying token

--- src/api/services/auth/auth.service.ts
+++ src/api/services/auth/auth.service.ts
@@ -4,10 +4,14 @@ import { isNullOrUndefined } from "util";
 
 export class AuthService {
     static VerifyToken(token: string) {
-        var retVal: string | null | object = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_KEY!)
-        if (isNullOrUndefined(retVal)) {
-            retVal = null;
+        try {
+            var retVal: string | null | object = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_KEY!)
+            if (isNullOrUndefined(retVal)) {
+                retVal = null;
+            }
+            return retVal
+        } catch (e) {
+            console.log(e)
         }
-        return retVal
     }
 }
